5c137505a022d9e8304539a81b563eeb24f6d42a
- <->
2024-01-28 04:29:37 +0600

    примерно бирнарсе болып жатыр

--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,32 @@
 public class Person {
     private static int id_gen = 1;
     private static int id;
+    private String name;
+    private String surname;
 
-    public String name;
-    public String surname;
 
     public Person(){
         id= id_gen++;
     }
-
-
-
-
+    public int getId(){
+        return id;
     }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public Person(String name,String surname){
+        this();
+        setName(name);
+        setSurname(surname);
+    }
+
 }
