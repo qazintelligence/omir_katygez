a395b9945d05fd743fe362c6bad22afc92b7af4d
- <->
2024-01-28 05:05:32 +0600

    интерфейске енди кошемин

--- src/Employee.java
+++ src/Employee.java
@@ -1,51 +1,51 @@
-//public class Employee {
-//    private static int id_gen = 1;
-//    private static int id;
-//    private String name;
-//    private String surname;
-//    private String position;
-//    private double salary;
-//
-//    public Employee(){
-//        id= id_gen++;
-//    }
-//    public int getId(){
-//        return id;
-//    }
-//    public void setName(String name){
-//        this.name = name;
-//    }
-//    public String getName(){
-//        return name;
-//    }
-//    public void setSurname(String surname){
-//        this.surname = surname;
-//    }
-//    public String getSurname(){
-//        return surname;
-//    }
-//    public void setPosition(String position){
-//        this.position = position;
-//    }
-//    public String getPosition(){
-//        return position;
-//    }
-//    public void setSalary(double salary){
-//        this.salary = salary;
-//    }
-//    public double getSalary(){
-//        return salary;
-//    }
-//    public Employee(String name,String surname,String position,double salary){
-//        this();
-//        setName(name);
-//        setSurname(surname);
-//        setPosition(position);
-//        setSalary(salary);
-//    }
-//
-//    @Override
-//    public String toString(){
-//        return "Employee:"+id+"."
-//    }
-//}
+public class Employee {
+    private static int id_gen = 1;
+    private static int id;
+    private String name;
+    private String surname;
+    private String position;
+    private double salary;
+
+    public Employee(){
+        id= id_gen++;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public void setPosition(String position){
+        this.position = position;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public Employee(String name,String surname,String position,double salary){
+        this();
+        setName(name);
+        setSurname(surname);
+        setPosition(position);
+        setSalary(salary);
+    }
+
+    @Override
+    public String toString(){
+        return "Employee:"+id+"."+name+" "+surname;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,52 @@
-//public class Student {
-//    private  int id;
-//
-//    }
-//}
-//
+public class Student {
+    private static int id_gen = 1;
+    private static int id;
+    private String name;
+    private String surname;
+    private double gpa;
+
+    public Student() {
+        id = id_gen++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public Student(String name, String surname, double gpa) {
+        this();
+        setName(name);
+        setSurname(surname);
+        setGpa(gpa);
+    }
+
+    @Override
+    public String toString() {
+        return "Student:"+id+"."+name+" "+ surname;
+    }
+}
